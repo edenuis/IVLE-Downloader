0041aac2836c13bbbdf6ed69081cd12a3ac6d584
edenuis <edenuis@gmail.com>
2018-08-18 13:10:25 +0800

    Update 1
    1) This program can run on any OS
    2) Added in checks for failed login

--- IVLE.py
+++ IVLE.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 class ModuleNotFoundError(Exception):
     pass
@@ -41,14 +42,38 @@ class IVLE():
         self.windows[self.count] = self.browser.window_handles[0]
         self.count += 1
         print("Proceeding to login to IVLE...")
-        username_input = WebDriverWait(self.browser, 30).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_userid']")))
+        username_input = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_userid']")))
         username_input.send_keys(self.user)
-        password_input = WebDriverWait(self.browser, 30).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_password']")))
+        password_input = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_password']")))
         password_input.send_keys(self.password)
-        sign_in_button = WebDriverWait(self.browser, 30).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_btnSignIn']")))
+        sign_in_button = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_btnSignIn']")))
         sign_in_button.click()
+        
+        while not self.check_login_status():
+            self.relogin()
+        
         print("Successfully login!")
         self.update()
+    
+    def relogin(self):
+        self.user = input("Enter NUS IVLE Username: ")
+        self.password = getpass.getpass("Enter NUS IVLE Password: ")
+        username_input = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_userid']")))
+        username_input.clear()
+        username_input.send_keys(self.user)
+        password_input = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_password']")))
+        password_input.clear()
+        password_input.send_keys(self.password)
+        sign_in_button = WebDriverWait(self.browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_btnSignIn']")))
+        sign_in_button.click()
+        
+    def check_login_status(self):
+        try:
+            failed_login = WebDriverWait(self.browser, 2).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='ctl00_ctl00_ContentPlaceHolder1_spnErrorMsg']")))
+            print(failed_login.text)  
+            return False
+        except TimeoutException:
+            return True
         
     def update(self):
         try:
@@ -71,19 +96,16 @@ class IVLE():
     
     def retrieve_module(self):
         for mod in self.browser.find_elements_by_xpath("//*[@id='collapseTwo']/div/div/div/div/div/strong/u/a"):
-            if "SCI" not in mod.text:
-                module_path = self.check_folder(mod.text)
-                module_folder_url = mod.get_attribute("href")
+            module_path = self.check_folder(mod.text)
+            module_folder_url = mod.get_attribute("href")
                 
-                if module_folder_url != None:
-                    if mod.text not in self.modules:
-                        self.modules[mod.text] = {}
-                    self.modules[mod.text]["url"] = module_folder_url
-                    self.modules[mod.text]["path"] = module_path
-                else:
-                    raise ModuleURLNotFoundError(mod.text + " URL not found!")
+            if module_folder_url != None:
+                if mod.text not in self.modules:
+                    self.modules[mod.text] = {}
+                self.modules[mod.text]["url"] = module_folder_url
+                self.modules[mod.text]["path"] = module_path
             else:
-                break
+                raise ModuleURLNotFoundError(mod.text + " URL not found!")
             
     def open_new_window(self, url):
         if self.count not in self.windows:
@@ -166,36 +188,76 @@ class IVLE():
                 
     def end(self):
         self.browser.close()
-'''
-def create_path(path):
-    if   
-'''
+
+def create_path(base, path_list):
+    for path in path_list:
+        base = os.path.join(base, path)
+    return base
+
+def create_directory(base, path_list):
+    path = base
+    for directory in path_list:
+        path = os.path.join(path, directory)
+        if not os.path.exists(path):
+            os.mkdir(path)
+    
 if __name__ == "__main__":
     print("This IVLE Python Program was built using Python 3.6 and runs only on Windows OS\n")
     print("This program will login to the user's NUS IVLE account and download all possible updates.")
-    print("The user only needs to provide an absolute path to store all the downloads and the program will create the necessary folders to store the files, if there is any.\n")
+    print("The user only needs to provide an absolute path to store all the downloads and the program will create the necessary folders to store the files, if there is any. The program will create a NUS folder in the designated download path to store all the downloads.\n")
     print("Lastly, this program also works as an updater. If this program had successfully ran on the computer, by providing the old absolute download path, this program will only check and download new files.\n")
     print("------------------------------------------------------------------------------------------")
     print("Please make sure you satisfy the following conditions before proceeding:")
-    print("1) Running this program on Windows OS")
-    print("2) Have already installed Python 3.x")
-    print("3) Have already installed the Python Module Selenium")
-    print("4) Have already downloaded Chromedriver.exe (2.4.1) for Selenium")
+    print("1) Have already installed Python 3.x")
+    print("2) Have already installed the Python Module Selenium")
+    print("3) Have already downloaded the correct Chromedriver.exe that matches own chrome's version")
     print("------------------------------------------------------------------------------------------\n")
     
+    print("NOTE: This program does not save any usernames, passwords and paths.")
+    
+    condition = input("Have you satisfied the above conditions: [Y(es)/N(o)] ").upper()
     while True:
-        condition = input("Have you satisfied the above conditions: [Y(es)/N(o)] ")
         if condition == "Y":
             username = input("Enter NUS IVLE Username: ")
             password = getpass.getpass("Enter NUS IVLE Password: ")
-            print("A windows absolute path is of this form: C:\\xxxx\\xxxx\\xxxx")
-            download_path = input("Enter the absolute download path where you would like to store the files in: ")
-            default_download_path = input("Enter the default download path: ")
+            
+            base_dir = os.path.expanduser("~")
+            
+            store_example = os.path.join(os.path.join(base_dir, "Desktop"), "NUS")
+        
+            store_dir_list = input("Where would you like to store the files in? (E.g. To store the files in " + store_example + ", enter 'Desktop NUS' without the single quotes. Reminder: The program will create an NUS folder on its own: ").split(" ")
+            store_dir_list.append("NUS")
+            download_path = create_path(base_dir, store_dir_list)
+            
+            while not os.path.exists(download_path):
+                condition = input(download_path + " does not exist. Do you wish to create this directory instead? [Y(es)/N(o)] ").upper()
+                while condition != "Y" and condition != "N":
+                    condition = input("Enter only Y/N: ").upper()
+                if condition == "Y":
+                    print("Creating directory " + download_path + "...")
+                    create_directory(base_dir, store_dir_list)
+                    print(download_path + " has been successfully created!\n")
+                elif condition == "N":
+                    store_dir_list = input("Where would you like to store the files in? ").split(" ")
+                    store_dir_list.append("NUS")
+                    download_path = create_path(base_dir, store_dir_list)
+            
+            default_download_path = os.path.join(base_dir, "Downloads")
             while not os.path.exists(default_download_path):
-                default_download_path = input("Default download path does not exist! Please check and enter the correct default download path: ")
-            chromedriver_path = input("Enter the absolute path where chromedriver.exe is stored in: ")
-            os.chdir(chromedriver_path)
-            chromedriver = chromedriver_path + "\\chromedriver.exe"
+                default_example = os.path.join(os.path.join(base_dir, "Desktop"), "Downloads")
+                default_download_dir_list = input(default_download_path + " does not exist. Please enter the correct default download path. E.g. If the default download path is " + default_example + ", enter 'desktop downloads' without the single quotes: ")
+                default_download_path = create_path(base_dir, default_download_dir_list)
+            
+            driver_example = os.path.join(os.path.join(base_dir, "Desktop"), "chromedriver")
+            driver_list = input("Enter the location where chromedriver.exe is stored in. E.g. If chromedriver.exe is store in " + driver_example + ", enter 'Desktop chromedriver' without the single quotes: ").split(" ")
+            chromedriver_path = os.path.join(create_path(base_dir, driver_list), "chromedriver.exe")
+            while not os.path.isfile(chromedriver_path):
+                driver_list = input("chromedriver.exe not found in " + chromedriver_path + ". Please enter where chromedriver.exe is stored in. E.g. If chromedriver.exe is stored in " + driver_example + ", enter 'Desktop chromedriver' without the single quotes.: ").split(" ")
+                chromedriver_path = os.path.join(create_path(base_dir, driver_list), "chromedriver.exe")
+            
+            os.chdir(create_path(base_dir, driver_list))
+            chromedriver = chromedriver_path
+            
             opts = Options()
             opts.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36")
             chrome = webdriver.Chrome(chromedriver, chrome_options=opts)
@@ -206,7 +268,7 @@ if __name__ == "__main__":
             print("Goodbye!")
             break
         else:
-            print("Wrong input! Only enter Y/N.")
+            condition = input("Wrong input! Only enter Y/N: ").upper()
 
         
         
